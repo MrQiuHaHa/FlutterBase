1416f364310191b789a53f78c213b961d098f4af
邱俊荣 <qiujunrong@LocalHostName.local>
2020-03-02 14:31:20 +0800

    抽屉边栏效果

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,9 @@ class App extends StatelessWidget
       debugShowCheckedModeBanner: false,
       home: Home(),
       theme: ThemeData(
-          primarySwatch: Colors.green
+          primarySwatch: Colors.green,
+          highlightColor: Color.fromRGBO(255, 255, 255, 0.5),
+          splashColor: Colors.white70
       )
     );
   }
@@ -30,35 +32,53 @@ class Home extends StatelessWidget
       child: Scaffold(
         backgroundColor: Colors.grey[100],
         appBar: AppBar(
-          leading: IconButton(
+            leading: IconButton(
             icon: Icon(Icons.menu), 
             tooltip: 'Navigation',
             onPressed: () => debugPrint('Navigation button is pressed.')
-          ),
-          title:Text('FLUTTER APP',style: TextStyle(color:Colors.white)),
-          actions: <Widget>[
-            IconButton(
+            ),
+            title:Text(
+              'FLUTTER APP',
+              style: TextStyle(color:Colors.white)
+            ),
+            actions: <Widget>[
+             IconButton(
               icon: Icon(Icons.search),
               tooltip: 'Search',
               onPressed: () => debugPrint('Search button is pressed.')
-            )
-          ],
-          elevation: 0.0,
-          bottom: TabBar(
-            tabs: <Widget>[
+             )
+            ],
+            elevation: 0.0,
+            bottom: TabBar(
+             unselectedLabelColor: Colors.black38,
+             indicatorColor: Colors.black54,
+             indicatorSize: TabBarIndicatorSize.label,
+             indicatorWeight: 1.0,
+             tabs: <Widget>[
                 Tab(icon:Icon(Icons.local_florist)),
                 Tab(icon:Icon(Icons.change_history)),
                 Tab(icon:Icon(Icons.directions_bike))
               ]
             ),
         ),
+        // body:ListViewDemo()
         body: TabBarView(
           children: <Widget>[
             Icon(Icons.local_florist,size:128.0, color:Colors.black12),
             Icon(Icons.change_history,size:128.0, color:Colors.black12),
             Icon(Icons.directions_bike,size:128.0, color:Colors.black12),
           ]
-        )
+        ),
+        drawer: Container(
+          color:Colors.white,
+          padding:EdgeInsets.all(8.0),
+          child:Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('This is a drawer !')
+            ],
+          )
+        ),
       )
     );
   }
