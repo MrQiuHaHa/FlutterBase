0ef50f1c2cb41eb70c36d7bf44fb30c3c31223a7
邱俊荣 <qiujunrong@LocalHostName.local>
2020-03-02 13:58:22 +0800

    TabbarController使用

--- lib/main.dart
+++ lib/main.dart
@@ -25,34 +25,41 @@ class Home extends StatelessWidget
     @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
+    return DefaultTabController(
+      length: 3, 
+      child: Scaffold(
         backgroundColor: Colors.grey[100],
         appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.menu), 
+            tooltip: 'Navigation',
+            onPressed: () => debugPrint('Navigation button is pressed.')
+          ),
           title:Text('FLUTTER APP',style: TextStyle(color:Colors.white)),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              tooltip: 'Search',
+              onPressed: () => debugPrint('Search button is pressed.')
+            )
+          ],
           elevation: 0.0,
+          bottom: TabBar(
+            tabs: <Widget>[
+                Tab(icon:Icon(Icons.local_florist)),
+                Tab(icon:Icon(Icons.change_history)),
+                Tab(icon:Icon(Icons.directions_bike))
+              ]
+            ),
         ),
-        body: ListViewDemo()
-      );
+        body: TabBarView(
+          children: <Widget>[
+            Icon(Icons.local_florist,size:128.0, color:Colors.black12),
+            Icon(Icons.change_history,size:128.0, color:Colors.black12),
+            Icon(Icons.directions_bike,size:128.0, color:Colors.black12),
+          ]
+        )
+      )
+    );
   }
 }
-
-
-
-// class Hello extends StatelessWidget
-// {
-//   @override
-//   Widget build(BuildContext context) {
-//     // TODO: implement build
-//     return Center(
-//       child: Text(
-//           'hello world !',
-//           textDirection: TextDirection.ltr,
-//           style: TextStyle(
-//             fontSize:40,
-//             fontWeight:FontWeight.bold,
-//             color:Colors.red
-//           )
-//       )
-//     );
-//   }
-// }
