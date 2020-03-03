35885d1f1ed0b4f85cfe48f44211c76e2b16d590
邱俊荣 <qiujunrong@LocalHostName.local>
2020-03-03 20:10:49 +0800

    day_3:基础部件的使用

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import './demo/Drawer_demo.dart';
 import './demo/bottom_navigation_bar_demo.dart';
 import './demo/listview_demo.dart';
+import './demo/basic_demo.dart';
 
 void main () => runApp(App()); 
 
@@ -68,7 +69,7 @@ class Home extends StatelessWidget
         body: TabBarView(
           children: <Widget>[
             ListViewDemo(),
-            Icon(Icons.change_history,size:128.0, color:Colors.black12),
+            BasicDemo(),
             Icon(Icons.directions_bike,size:128.0, color:Colors.black12),
           ]
         ),
