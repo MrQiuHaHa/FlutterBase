78ef2e9477197ae5df4eef661e6a0452ef027960
邱俊荣 <qiujunrong@LocalHostName.local>
2020-03-08 21:28:03 +0800

    路由跳转知识

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import './demo/basic_demo.dart';
 import './demo/layout_demo.dart';
 import './demo/view_demo.dart';
 import './demo/sliver_demo.dart';
+import './demo/navigator_demo.dart';
 
 void main () => runApp(App()); 
 
@@ -19,6 +20,11 @@ class App extends StatelessWidget
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Home(),
+      // initialRoute: '/',
+      routes: {
+        // '/':(context) => NavigatorDemo(),
+        '/about':(context) => Page(title: 'About',),
+      },
       theme: ThemeData(
           primarySwatch: Colors.green,
           highlightColor: Color.fromRGBO(255, 255, 255, 0.5),
