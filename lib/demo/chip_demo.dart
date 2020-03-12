f63a897d0872b912c676cea7a328f5363f0faefe
邱俊荣 <>
2020-03-12 13:56:21 +0800

    各种chip小固件的各种场景的实用

--- lib/demo/chip_demo.dart
+++ lib/demo/chip_demo.dart
@@ -6,6 +6,19 @@ class ChipDemo extends StatefulWidget {
 }
 
 class _ChipDemoState extends State<ChipDemo> {
+
+  List<String> _tags = [
+    'Apple',
+    'Banana',
+    'Lemon'
+  ];
+
+  String _selectTag = 'None';
+
+  List<String> _selectList = [];
+
+  String _choice = 'Lemon';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,12 +38,10 @@ class _ChipDemoState extends State<ChipDemo> {
                 Chip(
                   label: Text('Life'),
                 ),
-
                 Chip(
                   label: Text('Sunset'),
                   backgroundColor: Colors.orange,
                 ),
-
                 Chip(
                   label: Text('JunRong'),
                   backgroundColor: Colors.yellow,
@@ -39,7 +50,6 @@ class _ChipDemoState extends State<ChipDemo> {
                     child: Text('邱'),
                   ),
                 ),
-
                 Chip(
                   label: Text('TaoZi'),
                   avatar: CircleAvatar(
@@ -54,16 +64,125 @@ class _ChipDemoState extends State<ChipDemo> {
                     child: Text('邱'),
                   ),
                 ),
+                Chip(
+                  label: Text('City'),
+                  onDeleted: (){
+
+                  },
+                  // deleteIcon: Icon(Icons.delete),
+                  // deleteIconColor: Colors.redAccent,
+                  // deleteButtonTooltipMessage: 'Remove this tag',
+                ),
                 Divider(
                   color: Colors.grey[400],
                   height: 16,
-                  indent: 30,
-                )
+                  // indent: 30,
+                ),
+                Wrap(
+                  spacing: 8.0,
+                  children: _tags.map((tag){
+                    return Chip(
+                      label: Text(tag),
+                      onDeleted: () {
+                        setState(() {
+                          _tags.remove(tag);
+                        });
+                      },
+                    );
+                  }).toList(),
+                ),
+                Divider(
+                  color: Colors.grey[400],
+                  height: 16,
+                  // indent: 30,
+                ),
+                Container(
+                  width: double.infinity,
+                  child: Text('ActionChip: $_selectTag'),
+                ),
+                Wrap(
+                  spacing: 8.0,
+                  children: _tags.map((tag){
+                    return ActionChip(
+                      label: Text(tag),
+                      onPressed: () {
+                        setState(() {
+                          _selectTag = tag;
+                        });
+                      },
+                    );
+                  }).toList(),
+                ),
+                Divider(
+                  color: Colors.grey[400],
+                  height: 16,
+                  // indent: 30,
+                ),
+                Container(
+                  width: double.infinity,
+                  child: Text('FilterChip: ${_selectList.toString()}'),
+                ),
+                Wrap(
+                  spacing: 8.0,
+                  children: _tags.map((tag){
+                    return FilterChip(
+                      label: Text(tag),
+                      selected: _selectList.contains(tag),
+                      onSelected: (value) {
+                        setState(() {
+                          if (_selectList.contains(tag)) {
+                            _selectList.remove(tag);
+                          } else {
+                            _selectList.add(tag);
+                          }
+                        });
+                      },
+                    );
+                  }).toList(),
+                ),
+                Divider(
+                  color: Colors.grey[400],
+                  height: 16,
+                  // indent: 30,
+                ),
+                Container(
+                  width: double.infinity,
+                  child: Text('ChoiceChip: $_choice'),
+                ),
+                Wrap(
+                  spacing: 8.0,
+                  children: _tags.map((tag){
+                    return ChoiceChip(
+                      label: Text(tag),
+                      selectedColor: Colors.black,
+                      selected: _choice == tag,
+                      onSelected: (value) {
+                        setState(() {
+                          _choice = tag;
+                        });
+                      },
+                    );
+                  }).toList(),
+                ),
 
               ],
             )
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.restore),
+        onPressed: (){
+          setState(() {
+            _tags = [
+              'Apple',
+              'Banana',
+              'Lemon'
+            ];
+            _selectList = [];
+            _choice = 'Lemon';
+          });
+        }
       ),
     );
   }
