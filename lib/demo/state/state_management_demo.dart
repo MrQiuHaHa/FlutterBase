0357f9d4095e2ed7de6dd0ede8951a7d3e8b7ef0
邱俊荣 <>
2020-03-12 19:07:56 +0800

    数据多层传递的方式处理2，使用第三方对象（推荐）

--- lib/demo/state/state_management_demo.dart
+++ lib/demo/state/state_management_demo.dart
@@ -1,41 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 
-class StateManagementDemo extends StatefulWidget {
-  @override
-  _StateManagementDemoState createState() => _StateManagementDemoState();
-}
 
-class _StateManagementDemoState extends State<StateManagementDemo> {
-
-  int _count = 0;
-
-  void _increaseCount () {
-    setState(() {
-      _count++;
-    });
-  }
-
+class StateManagementDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return CounterProvider(
-      count: _count,
-      increaseCount: _increaseCount,
+    return ScopedModel(
+      model: CounterModel(),
       child: Scaffold(
         appBar: AppBar(
           title: Text('StateManagementDemo'),
           elevation: 0.0,
         ),
         body: CounterWrap(),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          backgroundColor: Colors.red,
-          onPressed: () {
-            setState(() {
-              _count += 1;
-            });
-          }
-        ),
+        floatingActionButton: ScopedModelDescendant<CounterModel>(
+          rebuildOnChange: false,
+          builder: (context, _, model) {
+          return FloatingActionButton(
+            child: Icon(Icons.add),
+            backgroundColor: Colors.red,
+            onPressed: model.increaseCount
+          );
+        }), 
       ),
     );
   }
@@ -55,12 +41,15 @@ class Counter extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final int count = CounterProvider.of(context).count;
-    final VoidCallback increaseCount = CounterProvider.of(context).increaseCount;
-    return ActionChip(
-      label: Text('$count'), 
-      onPressed: increaseCount,
-    );
+
+    return ScopedModelDescendant<CounterModel>(
+      rebuildOnChange: true,
+      builder: (context, _, model) {
+        return ActionChip(
+          label: Text('${model.count}'), 
+          onPressed: model.increaseCount,
+        );
+      }); 
   }
 }
 
