4f0911fd970818b15db72869db2ffdd8e55b9085
邱俊荣 <>
2020-03-12 18:50:57 +0800

    数据多层传递的方式处理1

--- lib/demo/state/state_management_demo.dart
+++ lib/demo/state/state_management_demo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 class StateManagementDemo extends StatefulWidget {
   @override
@@ -8,52 +9,84 @@ class StateManagementDemo extends StatefulWidget {
 class _StateManagementDemoState extends State<StateManagementDemo> {
 
   int _count = 0;
+
+  void _increaseCount () {
+    setState(() {
+      _count++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('StateManagementDemo'),
-        elevation: 0.0,
-      ),
-      body: Counter(
-        count: _count, 
-        increaseCount: () {
-          setState(() {
-            _count++;
-          });
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        backgroundColor: Colors.red,
-        onPressed: () {
-          setState(() {
-            _count += 1;
-          });
-        }
+    return CounterProvider(
+      count: _count,
+      increaseCount: _increaseCount,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('StateManagementDemo'),
+          elevation: 0.0,
+        ),
+        body: CounterWrap(),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          backgroundColor: Colors.red,
+          onPressed: () {
+            setState(() {
+              _count += 1;
+            });
+          }
+        ),
       ),
     );
   }
 }
 
-class Counter extends StatelessWidget {
-  final int count;
-  final VoidCallback increaseCount;
-
-  Counter({
-    this.count,
-    @required this.increaseCount
-  });
+class CounterWrap extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: ActionChip(
-          label: Text('$count'), 
-          onPressed: () {
-            increaseCount();
-          },
-        ),
+      child: Counter(),
     );
+  }
+}
+
+class Counter extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    final int count = CounterProvider.of(context).count;
+    final VoidCallback increaseCount = CounterProvider.of(context).increaseCount;
+    return ActionChip(
+      label: Text('$count'), 
+      onPressed: increaseCount,
+    );
+  }
+}
+
+class CounterProvider extends InheritedWidget {
+  CounterProvider({this.child,this.count,this.increaseCount}) : super(child: child);
+
+  final Widget child;
+  final int count;
+  final VoidCallback increaseCount;
+
+  static CounterProvider of(BuildContext context) {
+    return (context.dependOnInheritedWidgetOfExactType(aspect: CounterProvider));
+  }
+
+  @override
+  bool updateShouldNotify( CounterProvider oldWidget) {
+    return true;
+  }
+}
+
+class CounterModel extends Model {
+  int _count = 0;
+  int get count => _count;
+
+  void increaseCount () {
+    _count += 1;
+    notifyListeners();
   }
 }
