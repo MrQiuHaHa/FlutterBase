415db8f6036b2ee22a3a6a314d8ae40483fb8af7
邱俊荣 <>
2020-03-14 14:30:12 +0800

    Block的回调方式

--- lib/demo/bloc/bloc_demo.dart
+++ lib/demo/bloc/bloc_demo.dart
@@ -1,23 +1,20 @@
 import 'package:flutter/material.dart';
+import 'counter_bloc_demo.dart';
+
 
 class BlocDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BlocDemo'),
-        elevation: 0.0,
-      ),
-      body: BlocDemoHome(),
-    );
-  }
-}
-
-class BlocDemoHome extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      
+    return CounterProvider(
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text('BlocDemo'),
+              elevation: 0.0,
+            ),
+            body: CounterHome(),
+            floatingActionButton: CounterActionButton(),
+          ),
+          bloc: CounterBloc(),
     );
   }
 }
