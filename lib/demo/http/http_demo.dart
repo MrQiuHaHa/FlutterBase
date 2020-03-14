20bc75a8878b89360036a2af18e31c90dd65ab09
邱俊荣 <>
2020-03-14 15:37:56 +0800

    安装http请求的package

---
+++ lib/demo/http/http_demo.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HttpDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HttpDemo'),
+        elevation: 0.0,
+      ),
+      body: HttpDemoHome(),
+    );
+  }
+}
+
+class HttpDemoHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
