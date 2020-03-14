bc459a024884cd7d096b1f87dec011c220c64ebd
邱俊荣 <>
2020-03-14 17:28:17 +0800

    网络请求处理

--- lib/demo/http/http_demo.dart
+++ lib/demo/http/http_demo.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_app/model/post.dart';
 
 import 'package:http/http.dart' as http;
 import 'dart:async';
@@ -28,34 +27,42 @@ class _HttpDemoHomeState extends State<HttpDemoHome> {
 
   @override
   void initState() {
-    // _fetchPost();
+    fetchPosts().then((onvalue) {
+      print(onvalue);
+    });
 
-    final post = {
-      'title': 'hello',
-      'description': 'nice to meet you'
-    };
-    debugPrint(post['title']);
-    debugPrint(post['description']);
+    // final post = {
+    //   'title': 'hello',
+    //   'description': 'nice to meet you'
+    // };
+    // debugPrint(post['title']);
+    // debugPrint(post['description']);
 
-    final jsonPost = jsonEncode(post);//map结构转jsonstring，这样才能传给后端
-    debugPrint(jsonPost);
+    // final jsonPost = jsonEncode(post);//map结构转jsonstring，这样才能传给后端
+    // debugPrint(jsonPost);
 
-    final mapPost = jsonDecode(jsonPost);//jsonString转map
-    debugPrint('取字典标题${mapPost['title']}');
-    debugPrint(mapPost['description']);
+    // final mapPost = jsonDecode(jsonPost);//jsonString转map
+    // debugPrint('取字典标题${mapPost['title']}');
+    // debugPrint(mapPost['description']);
 
-    final postModel = Post.fromJson(mapPost);//map转模型
-    debugPrint('title: ${postModel.title}, description: ${postModel.description}');
+    // final postModel = Post.fromJson(mapPost);//map转模型
+    // debugPrint('title: ${postModel.title}, description: ${postModel.description}');
 
-    debugPrint('${jsonEncode(postModel)}');//这个方法会自动触发Post里写的tojson方法转map,再完成模型转jsonstring
+    // debugPrint('${jsonEncode(postModel)}');//这个方法会自动触发Post里写的tojson方法转map,再完成模型转jsonstring
 
     super.initState();
   }
 
-  _fetchPost() async {
+  Future<List<Post>> fetchPosts() async {
     final response = await http.get('https://resources.ninghao.net/demo/posts.json');
-    debugPrint('statusCode: ${response.statusCode}');
-    debugPrint('body: ${response.body}');
+    
+    if (response.statusCode == 200) {
+      final responseBody = jsonDecode(response.body);
+      List<Post> posts = responseBody['posts'].map<Post>((item) => Post.fromJson(item)).toList();
+      return posts;
+    } else {
+      throw Exception('some thing is wrong');
+    }
   }
 
   @override
@@ -70,17 +77,32 @@ class Post {
 
   final String title;
   final String description;
+  final String id;
+  final String author;
+  final String imageUrl;
 
   Post(
     this.title,
-    this.description
+    this.description,
+    this.id,
+    this.author,
+    this.imageUrl
   );
 
   Post.fromJson(Map json)
    : title = json['title'],
-     description = json['description'];
+     description = json['description'],
+     id = json['id'].toString(),
+     author = json['author'],
+     imageUrl = json['imageUrl'];
 
   Map toJson() {
-    return {'title':title,'description':description};
+    return {
+      'title':title,
+      'description':description,
+      'author':author,
+      'imageUrl':imageUrl,
+      'id':id
+    };
   }
 }
