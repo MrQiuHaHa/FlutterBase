d18952276d94e6f35349b33798a36ff39aa6819b
邱俊荣 <>
2020-03-14 13:13:03 +0800

    bloc，谷歌响应式业务开发组件

---
+++ lib/demo/bloc/bloc_demo.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class BlocDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BlocDemo'),
+        elevation: 0.0,
+      ),
+      body: BlocDemoHome(),
+    );
+  }
+}
+
+class BlocDemoHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
