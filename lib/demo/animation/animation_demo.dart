ad6e8588b90c4a1733d1408c362289bed91ed5d5
邱俊荣 <>
2020-03-15 14:43:35 +0800

    AnimationController小固件使用

--- lib/demo/animation/animation_demo.dart
+++ lib/demo/animation/animation_demo.dart
@@ -18,11 +18,56 @@ class AnimationDemoHome extends StatefulWidget {
   _AnimationDemoHomeState createState() => _AnimationDemoHomeState();
 }
 
-class _AnimationDemoHomeState extends State<AnimationDemoHome> {
+class _AnimationDemoHomeState extends State<AnimationDemoHome> with TickerProviderStateMixin {
+
+  AnimationController animationDemoController;
+
+  @override
+  void initState() {
+
+    animationDemoController = AnimationController(
+      value: 30,
+      lowerBound: 10.0,
+      upperBound: 100,
+      duration: Duration(seconds: 1),
+      vsync: this
+    );
+
+    animationDemoController.addListener(() {
+      print('${animationDemoController.value}');//打印次数代表1秒内多少帧，当前设置了1秒内的帧率
+      setState(() {
+        
+      });
+    });
+    animationDemoController.addStatusListener((AnimationStatus status) {
+      print(status);
+    });
+    // animationDemoController.forward();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    animationDemoController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Center(
+      child: IconButton(
+        icon: Icon(Icons.favorite),
+        iconSize: animationDemoController.value, 
+        onPressed: (){
+          switch (animationDemoController.status) {
+            case AnimationStatus.completed:
+              animationDemoController.reverse();
+              break;
+            default:
+              animationDemoController.forward();
+          }
+        }
+      ),
     );
   }
 }
